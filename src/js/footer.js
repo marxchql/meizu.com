265230fcccf30e396586fded5f6bda41947c6203
xiaoqilong <1964577717@qq.com>
2020-08-28 21:44:14 +0800

    v 1.0.3

---
+++ src/js/footer.js
@@ -0,0 +1,70 @@
+let footer = `
+<div class="footerWrp">
+<div class="footer_Left">
+    <ul class="top">
+        <li>
+            <img src="../img/footer/jisu.png" alt=""></n>
+            <p> <a href="">急速服务</a></p>
+        </li>
+        <li>
+            <img src="../img/footer/shangmen.png" alt=""></n>
+            <p> <a href="">上门服务</a></p>
+        </li>
+        <li>
+            <img src="../img/footer/weixin.png" alt=""></n>
+            <p> <a href="">微信客服</a></p>
+        </li>
+        <li>
+            <img src="../img/footer/weixiu.png" alt=""></n>
+            <p> <a href="">快速维修</a></p>
+        </li>
+        <li>
+            <img src="../img/footer/tuihuo.png" alt=""></n>
+            <p> <a href="">在线退货</a></p>
+        </li>
+        <li>
+            <img src="../img/footer/lainjie.png" alt=""></n>
+            <p> <a href="">维修链接</a></p>
+        </li>
+    </ul>
+    <p>
+        <a href="">了解我们</a>
+        <a href="">加入我们</a>
+        <a href="">练习我们</a>
+        <a href="">Flyme</a>
+        <a href="">魅族社区</a>
+        <a href="">天猫旗舰店</a>
+        <a href="">问题反馈</a>
+        <a href="">线上销售授权名单</a>
+        <a href="">出版经营许可证</a>
+        <a href="">简体中文</a>
+
+        <a href="">©2019 Meizu Telecom Equipment Co., Ltd. All rights reserved.  粤ICP备13003602号合字B2-20170010  营业执照法律声明 粤公网安备 44049102496009 号 </a>
+    </p>
+</div>
+<div class="footer_right">
+    <p>周一至周日 8:00-20:00</p>
+    </n>
+    <span>400-788-3333</span>
+    <div class="console">
+        在线客服
+    </div>
+
+    <ul>
+        <li>
+            <img src="../img/footer/weixin.png" alt="">
+        </li>
+        <li>
+            <img src="../img/footer/lainjie.png" alt="">
+        </li>
+        <li>
+            <img src="../img/footer/weixin.png" alt="">
+        </li>
+        <li>
+            <img src="../img/footer/lainjie.png" alt="">
+        </li>
+    </ul>
+</div>
+</div>
+`
+export { footer };
---
+++ src/js/header.js
@@ -0,0 +1,36 @@
+let header = `
+        <nav>
+            <span class="iconfont icon-meizu"></span>
+            <ul>
+                <li><a href="">手机</a></li>
+                <li><a href="">声学</a></li>
+                <li><a href="">配件</a></li>
+                <li><a href="">生活</a></li>
+                <li><a href="">Flyme</a></li>
+                <li><a href="">服务</a></li>
+                <li><a href="">专卖店</a></li>
+                <li><a href="">社区</a></li>
+                <li><a href="">政企定制</a></li>
+                <li>APP下载
+                    <div class="appDown">
+                        <img src="../img/APPdown1.jpg" alt="">
+                    </div>
+                </li>
+            </ul>
+            <span class="search">
+                <input type="text" placeholder="魅族17Pro">
+                <span class="iconfont icon-fangdajing">
+                </span>
+            </span>
+
+            <span class="iconfont icon-wode">
+                <ul class="user">
+                <li><a href="../html/login.html" target="_blank">立即登录</a></li>
+                <li><a href="../html/register.html" target="_blank">立即注册</a></li>
+                <li><a href="">我的订单</a></li>
+                <li><a href="">M码通道</a></li>
+            </ul></span>
+            <span class="iconfont icon-gouwuche"></span>
+        </nav>
+`
+export { header };
